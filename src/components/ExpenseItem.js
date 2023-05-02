2efc2ebaa7f20743a7c7d04ad075e77abe40270b
Charlotte Griffin <charlottegriffin@Charlottes-MacBook-Pro.local>
2023-05-02 19:11:37 +0100

    made a separate expenses file to contain the individual expenses and passed the array of objects from app.js to expenses.js as a prop

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -7,7 +7,7 @@ const ExpenseItem = (props) => {
 
   return (
     <div className="expense-item">
-      <ExpenseDate />
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">£{props.amount}</div>
