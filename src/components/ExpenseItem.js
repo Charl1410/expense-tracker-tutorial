b55d50d07c2357e10f4f24f32a9df690a08b1b8f
Charlotte Griffin <charlottegriffin@Charlottes-MacBook-Pro.local>
2023-05-02 17:25:49 +0100

    turned date into its own component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './ExpenseItems.css'
+import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
+ 
+
   return (
-    
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">£{props.amount}</div>
