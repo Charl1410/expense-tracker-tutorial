2b6a726421be9cfaa75824614c7013d79e9ed5f2
Charlotte Griffin <charlottegriffin@Charlottes-MacBook-Pro.local>
2023-05-03 14:22:11 +0100

    created a usestate function to update the value of title in each expense card

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { useState } from "react";
+
 import './ExpenseItem.css';
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
- 
+  //state to hold the value of the card expense and it can be updated by clicking the button 
+  const [title, setTitle] = useState(props.title);
+
+  const handleClick = () => {
+    setTitle('updated!');
+  }
 
   return (
     //Cannot put components within components unless using props.children 
@@ -13,9 +20,10 @@ const ExpenseItem = (props) => {
       onto ExpenseDate.js*/}
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">£{props.amount}</div>
       </div>
+      <button onClick={handleClick}>Change Title</button>
     </Card>
   );
 };
