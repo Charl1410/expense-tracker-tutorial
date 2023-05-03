594acbd086dd3b6021a2e44b8f538c5384afc3ce
Charlotte Griffin <charlottegriffin@Charlottes-MacBook-Pro.local>
2023-05-03 12:16:55 +0100

    adding card.js wrapper for css for expenses and expenseitems

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Card.css';
+
+const Card = (props) => {
+    //this adds the card classes in card.css but also needs the css from the other file 
+    const classes = 'card ' + props.className;
+  return (
+    // the value of the children prop will always be content between the opening and closing tags of the 
+    //custom component (the opening and closing tag) meaning that it is the content in ExpenseItems 
+    <div className={classes}>{props.children}</div>
+  )
+}
+
+export default Card
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,21 @@
 import React from "react";
 import './ExpenseItems.css'
 import ExpenseDate from "./ExpenseDate";
+import Card from './Card'
 
 const ExpenseItem = (props) => {
  
 
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
+      {/* we are prop drilling here because data is being passed from app.js to this file and then
+      onto ExpenseDate.js*/}
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">£{props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 };
 
