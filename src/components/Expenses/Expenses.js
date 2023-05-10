2414dd7081de09dbc5b67b401b0f4108674d8e77
Charlotte Griffin <charlottegriffin@Charlottes-MacBook-Pro.local>
2023-05-10 23:34:07 +0100

    put the expenseFilter comp inside of the expenses card, changed the passed in data from the filter to the expenses as state

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,32 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Expenses.css';
-import ExpenseItem from '../Expenses/ExpenseItem ';
+import ExpenseItem from '../Expenses/ExpenseItem';
 import Card from '../UI/Card';
+import ExpenseFilter from '../Expenses/ExpensesFilter'
 
 const Expenses = ({expenses}) => {
+  const [filteredYear, setFilteredYear] = useState('2020')
+  const saveYearHandler = (yearSelected) => {
+    setFilteredYear(yearSelected);
+
+
+  }
   return (
-    <Card className='expenses'>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
-    </Card>
+    <>
+      <Card className="expenses">
+        <ExpenseFilter selected={filteredYear} onSaveYearSelected={saveYearHandler} />
+
+        <ExpenseItem
+          title={expenses[0].title}
+          amount={expenses[0].amount}
+          date={expenses[0].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={expenses[1].title}
+          amount={expenses[1].amount}
+          date={expenses[1].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={expenses[2].title}
+          amount={expenses[2].amount}
+          date={expenses[2].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={expenses[3].title}
+          amount={expenses[3].amount}
+          date={expenses[3].date}
+        ></ExpenseItem>
+      </Card>
+    </>
   );
 }
 
--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import "./ExpensesFilter.css";
-
-
-//TODO create a filter section component that allows us to use a drop down and filter the year e.g. 2021
-
-//listen to changes on the dropdown 
-
-//whenever the user selects a new year from the dropdown listen to this event, make sure the picked year is 
-//forwarded through the expenses component 
-//add the expense filter component into the Expense js file 
-//Forward the picked year from the ExpenseFilter into the Expenses component 
-//in the expenses component store the picked year into the state 
+import { useState } from 'react';
 
 
 
-const ExpensesFilter = () => {
+const ExpensesFilter = (props) => {
+  const [yearSelected, setYearSelected] = useState('');
+
+  //TODO create a filter section component that allows us to use a drop down and filter the year e.g. 2021
+  const handleSelectChange = (event) => {
+    console.log(event.target.value)
+    setYearSelected(event.target.value);
+    props.onSaveYearSelected(yearSelected);
+  };
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select>
+        {/* listen to changes on the dropdown and pass the function to pull through the selected year to this comp */}
+        {/* The value prop sets the default value to the selected passed from the called component in expenses.js*/}
+        <select onChange={handleSelectChange}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
