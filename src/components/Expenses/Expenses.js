184365f23107aefe7d77a4a4a3d83558d458ba4b
Charlotte Griffin <charlottegriffin@Charlottes-MacBook-Pro.local>
2023-05-11 14:56:00 +0100

    added conditional render based on array length to expenses.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,22 +8,34 @@ const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020')
   const saveYearHandler = (yearSelected) => {
     setFilteredYear(yearSelected);
-
-
   }
+
+//create a function that take the whole array and filters the items based on the filteredYear 
+//this will return true if the year matches or false if it does not 
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <>
       <Card className="expenses">
-        <ExpenseFilter selected={filteredYear} onSaveYearSelected={saveYearHandler} />
-      {/* expense represents each item which is an object in the array then we define the key val pairs in the array  */}
-      {props.items.map(expense => (
-       <ExpenseItem 
-       key={expense.id}
-       title={expense.title}
-       amount={expense.amount}
-       date={expense.date}
+        <ExpenseFilter
+          selected={filteredYear}
+          onSaveYearSelected={saveYearHandler}
         />
-        ))}
+        {/* expense represents each item which is an object in the array then we define the key val pairs in the array  */}
+        {/* displaying the filtered expenses which takes the given array from the main array and has been filtered to the date */}
+        {filteredExpenses.length === 0 && <p>No expenses found</p>}
+        {filteredExpenses.length > 0 &&
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))}
+        
       </Card>
     </>
   );
