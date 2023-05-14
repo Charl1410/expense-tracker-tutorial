d48850b7b204e8d6799c4253fccebee6c5398b6d
Charlotte Griffin <charlottegriffin@Charlottes-MacBook-Pro.local>
2023-05-14 21:58:40 +0100

    wrapping card in li

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -10,16 +10,18 @@ const ExpenseItem = (props) => {
   //state to hold the value of the card expense and it can be updated by clicking the button 
 
   return (
-    //Cannot put components within components unless using props.children 
-    <Card className="expense-item">
-      {/* we are prop drilling here because data is being passed from app.js to this file and then
+    //Cannot put components within components unless using props.children
+    <li>
+      <Card className="expense-item">
+        {/* we are prop drilling here because data is being passed from app.js to this file and then
       onto ExpenseDate.js*/}
-      <ExpenseDate date={props.date}/>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">£{props.amount}</div>
-      </div>
-    </Card>
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">£{props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
 };
 
